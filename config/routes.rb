dc810668efb01f7f1dba7c7f1dac7fe1ddf082c1
agashi1234 <80380575+agashi1234@users.noreply.github.com>
2021-04-02 21:42:47 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'prototypes#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: :show
 end
